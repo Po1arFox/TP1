bdc905a5cbdf0d1968f3855fcdd27bd14a21ec74
Po1arFox <prolygin.dima@gmail.com>
2023-10-21 12:11:31 +0300

    Realized virtual methods of base class "Writer"

---
+++ TP1/Writer.cpp
@@ -0,0 +1,41 @@
+﻿#include "Writer.h"
+
+void Writer::add_book(string book)
+{
+	number_of_books++;
+	string* new_books = new string[number_of_books];
+	for (int i = 0; i < number_of_books - 1; i++) {
+		new_books[i] = name_books[i];
+	}
+	new_books[number_of_books - 1] = book;
+	name_books = new_books;
+}
+
+void Writer::delete_book()
+{
+	number_of_books--;
+	string* new_books = new string[number_of_books];
+	for (int i = 0; i < number_of_books; i++) {
+		new_books[i] = name_books[i];
+	}
+	delete[] name_books;
+	name_books = new_books;
+}
+
+
+string Writer::get_fullname()
+{
+	return fullname;
+}
+
+string* Writer::get_name_books()
+{
+	return name_books;
+}
+
+void Writer::set_fullname(string fullname)
+{
+	this->fullname = fullname;
+}
+
+
